cb4622d8b1a510f54437f58d5c6657a0acf9c4be
Alona1m <sergei.ua.kh@gmail.com>
2022-10-21 12:02:24 +0300

    Добавляет решение task-02

---
+++ js/task-02.js
@@ -0,0 +1,23 @@
+/* Напиши скрипт подсчета стоимости гравировки украшений. Для этого создай функцию
+calculateEngravingPrice(message, pricePerWord) принимающую строку(в строке будут только слова и пробелы)
+и цену гравировки одного слова, и возвращающую цену гравировки всех слов в строке. */
+
+const calculateEngravingPrice = function (message, pricePerWord) {
+
+  let wordСount = message.split(" ").length;
+  let price = wordСount * pricePerWord;
+
+  return price;
+}
+
+
+
+// Вызовы функции для проверки работоспособности.
+
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80 //
+  
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160 //
+  
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240 //
+  
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
